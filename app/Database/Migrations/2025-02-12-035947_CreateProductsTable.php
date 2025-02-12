69260b09095d51fa3bc3abb4f1b854f17ee8b113
Zxn1 <maintanance1991@gmail.com>
2025-02-12 12:16:47 +0800

    mulakan dengan migration

---
+++ app/Database/Migrations/2025-02-12-035947_CreateProductsTable.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProductsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField(
+            [
+                'id' => [
+                    'type' => 'INT',
+                    'constraint' => '11',
+                    'auto_increment' => true
+                ],
+                'name' => [
+                    'type' => 'VARCHAR',
+                    'constraint' => '255',
+                    'null' => true
+                ],
+                'description' => [
+                    'type' => 'TEXT',
+                    'null' => true
+                ],
+                'price' => [
+                    'type' => 'DECIMAL',
+                    'constraint' => '10,2',
+                    'default' => 0.00,
+                ],
+                'created_at' => [
+                    'type' => 'DATETIME',
+                    'null' => true,
+                ],
+                'updated_at' => [
+                    'type' => 'DATETIME',
+                    'null' => true,
+                ]
+            ]
+        );
+        
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('products');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('products');
+    }
+}
+
+
